abc1b46dafef7144d8b710e4ef2263b456246367
Maria Castro <mcastrod08@gmail.com>
2025-04-30 21:09:18 +0200

    desktop navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router'
+import { sidebarItems } from '~/constants'
+import { cn } from '~/lib/utils'
+
+const NavItems = () => {
+
+  const user = {
+    name: 'David',
+    email: 'david@gmail.com',
+    imgUrl: '/assets/images/david.webp'
+  }
+
+
+  return (
+    <section className="nav-items">
+      <Link to='/' className="link-logo">
+          <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]"/>
+          <h1>TourMC</h1>
+      </Link>
+
+      <div className="container">
+        <nav>
+          { sidebarItems.map(({id, href, icon, label}) => (
+            <NavLink to={href} key={id}>
+              {( {isActive } : { isActive: boolean }) => (
+                <div className={cn('group nav-item', {
+                  'bg-primary-100 !text-white' : isActive
+                })}>
+                  <img 
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-0 group-hover_invert ${isActive ? 'brightness-0 invert' : 'text-dark-200'}`}
+                  />
+                  {label}
+                </div>
+              )}
+            </NavLink>
+          )) }
+        </nav>
+
+        <footer className="nav-footer">
+          <img src={user?.imgUrl || '/assets/images/david.webp'} alt={user?.name || 'user'}/>
+          <article>
+            <h2>
+              {user?.name}
+            </h2>
+            <p>{user?.email}</p>
+          </article>
+          <button className="cursor-pointer "
+            onClick={() => {
+              console.log('logout')
+            }}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" className="size-6"/>
+
+          </button>
+        </footer>
+      </div>
+    </section>
+  )
+}
+
+export default NavItems
