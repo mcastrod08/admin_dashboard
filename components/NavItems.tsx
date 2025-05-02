0cd1765b8e9007e050bb753e5ea9064451804562
Maria Castro <mcastrod08@gmail.com>
2025-05-02 13:38:18 +0200

    mobile navbar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router'
 import { sidebarItems } from '~/constants'
 import { cn } from '~/lib/utils'
 
-const NavItems = () => {
+const NavItems = ({handleClick}:{ handleClick?: () => void }) => {
 
   const user = {
     name: 'David',
@@ -26,7 +26,7 @@ const NavItems = () => {
               {( {isActive } : { isActive: boolean }) => (
                 <div className={cn('group nav-item', {
                   'bg-primary-100 !text-white' : isActive
-                })}>
+                })} onClick={handleClick}>
                   <img 
                     src={icon}
                     alt={label}
