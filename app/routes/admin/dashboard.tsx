37d6c70f3fe2c3af5423d89ab7d3b0f32b0c05d8
Maria Castro <mcastrod08@gmail.com>
2025-05-03 13:05:17 +0200

    git

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,15 +1,63 @@
-import { Header } from 'components'
+import { Header, StatsCard, TripCard } from 'components'
 import React from 'react'
+import { dashboardStats, dashboardTrips, user } from "../../constants"
 
 const Dashboard = () => {
-  const user = { name: 'Maria' };
+  
+
+  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
+  
   return (
     <main className="dashboard wrapper">
       <Header 
         title={`Welcome ${user?.name ?? 'Guest'} `}
         description="aaaaaaaaaaaaaaaaaaaaaaaaaaaa"  
       />
-      Dashboard
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+        <StatsCard 
+            headerTitle="Total Users" 
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}  
+          />
+          
+          <StatsCard 
+            headerTitle="Total Trips" 
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}  
+          />
+  
+          <StatsCard 
+            headerTitle="Active Users" 
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}  
+          />
+        </div>
+      </section>qs
+
+      <section className="flex flex-col gap-6">
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+
+        <div className="trip-grid">
+          {dashboardTrips.slice(0,4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+            <TripCard 
+              key={id}
+              id={id.toString()}
+              name={name}
+              imageUrl={imageUrls[0]}
+              location={itinerary?.[0]?.location ?? 'No Itinerary'}
+              tags={tags}
+              price={estimatedPrice}
+            />
+          ))}
+        </div>
+
+      </section>
+      
+      
     </main>
   )
 }
